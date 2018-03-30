b13da6084977c6dc255cea6a11fa745fedba4620
Lisa Jetton <ldjetton21@gmail.com>
2018-03-30 17:50:08 -0500

    updates to friends.json list and grammar corrections

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@ var friendsList =
   "name":"Ahmed Haque",
   "photo":"https://media.licdn.com/media/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
   "scores":[
-      5,
+      2,
       4,
       5,
       3,
@@ -28,6 +28,66 @@ var friendsList =
       5,
       4,
       1    
+    ]
+},{
+"name": "Andy",
+"photo": "https://thoughtcatalog.files.wordpress.com/2013/01/andy-dwyer-andy-dwyer-20680671-500-279.jpg",
+"scores": [
+      4,
+      1,
+      3,
+      4,
+      5,
+      1,
+      2,
+      3,
+      3,
+      2
+    ]
+},{
+"name": "Leslie",
+"photo": "https://media1.popsugar-assets.com/files/thumbor/2ebfSQkPUKjxKfb5N3h2N-VIh7U/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2015/02/24/097/n/1922283/79e574c2_edit_img_image_845210_1424395524/i/Best-Leslie-Knope-GIFs.png",
+"scores": [
+      3,
+      1,
+      3,
+      2,
+      1,
+      1,
+      2,
+      2,
+      3,
+      4
+    ]
+},{
+"name": "Tom",
+"photo": "https://cdn.pastemagazine.com/www/blogs/lists/tomhaverford.png",
+"scores": [
+      4,
+      1,
+      2,
+      1,
+      1,
+      1,
+      2,
+      5,
+      3,
+      2
+    ]
+},{
+"name": "Ron",
+"photo": "http://i0.kym-cdn.com/entries/icons/original/000/023/463/6360103332924270401274957217_Ron.jpg",
+"scores": [
+      1,
+      1,
+      3,
+      3,
+      3,
+      1,
+      1,
+      2,
+      1,
+      2
     ]
 },{
   "name":"Happy Cassidy",
