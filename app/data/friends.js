6232951ad7eb6740881f48b70a8e7e76cb9f72ac
Lisa Jetton <ldjetton21@gmail.com>
2018-04-11 19:02:52 -0500

    json updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -59,6 +59,21 @@ var friendsList =
       3,
       2
     ]
-}]
+},{
+  "name":"Happy Cassidy",
+  "photo":"https://c-8oqtgrjgwu46x24efp4-yyyx2efqivkogx2eeqo.g00.dogtime.com/g00/3_c-8fqivkog.eqo_/c-8OQTGRJGWU46x24jvvrx3ax2fx2fefp4-yyy.fqivkog.eqox2fcuugvux2fwrnqcfux2ficnngtax2fiqnfgp-tgvtkgx78gt-fqiu-cpf-rwrrkgux2fiqnfgp-tgvtkgx78gt-fqiu-rwrrkgu-3.lrix3fk32e.octm.kocig.varg_$/$/$/$/$/$",
+  "scores":[
+      3,
+      1,
+      2,
+      1,
+      1,
+      2,
+      1,
+      1,
+      1,
+      5    
+    ]
+ }]
 
 module.exports = friendsList;
